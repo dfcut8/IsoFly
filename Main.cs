b8b093f9748918daed78ae11041a37aa8b255396
dfcut8 <dfcut8@proton.me>
2025-11-26 17:42:41 -0500

    actually something working

--- Main.cs
+++ Main.cs
@@ -5,57 +5,22 @@ public partial class Main : Node2D
     [Export]
     private TileMapLayer tileMapLayer;
 
-    [Export]
-    private bool showCoordinateOverlay = true;
-
-    [Export]
-    private Color coordinateColor = Colors.White;
-
-    [Export]
-    private int coordinateFontSize = 14;
-
-    [Export]
-    private float fallDuration = 2.0f;
-
-    [Export]
-    private float fallHeight = 500.0f;
-
-    private Font coordinateFont;
-
     public override void _Ready()
     {
-        // Load default font - will use system font
-        coordinateFont = new SystemFont();
-
-        // Get all tiles in the tilemap and print their names
-        GetTileScenes();
+        CallDeferred(MethodName.GetTileMapChildren);
     }
 
-    private void GetTileScenes()
+    private void GetTileMapChildren()
     {
-        if (tileMapLayer == null || tileMapLayer.TileSet == null)
-            return;
-
-        // Get all used cells
-        var usedCells = tileMapLayer.GetUsedCells();
-        GD.Print($"Total tiles in tilemap: {usedCells.Count}");
-
-        foreach (Vector2I cellCoords in usedCells)
+        var tmlChildren = tileMapLayer.GetChildren();
+        var delay = 0.0f;
+        foreach (var child in tmlChildren)
         {
-            // Get the source ID for this cell
-            int sourceId = tileMapLayer.GetCellSourceId(cellCoords);
-
-            // Get the source from the tileset
-            TileSetSource source = tileMapLayer.TileSet.GetSource(sourceId);
-
-            // Check if it's a TileSetScenesCollectionSource (which contains scenes)
-            // if (source is TileSetScenesCollectionSource sceneSource)
-            // {
-            //     int altId = tileMapLayer.GetCellAlternativeTile(cellCoords);
-            //     // The assigned PackedScene.
-            //     PackedScene scene = sceneSource.GetSceneTileScene(altId);
-            //     GD.Print(scene);
-            // }
+            if (child is Tile tile)
+            {
+                GetTree().CreateTimer(delay).Timeout += () => tile.PlayAnimation();
+                delay += 0.1f;
+            }
         }
     }
 }
--- Tile.cs
+++ Tile.cs
@@ -2,13 +2,19 @@ using Godot;
 
 public partial class Tile : Node2D
 {
+    private Vector2 finalPosition;
+
     public override void _Ready()
     {
-        var finalPos = Position;
-        Position = new Vector2(Position.X, Position.Y - 500);
+        finalPosition = Position;
+        Position = new Vector2(Position.X, Position.Y - 1000);
+    }
+
+    public void PlayAnimation()
+    {
         var tween = GetTree().CreateTween();
         tween.SetEase(Tween.EaseType.In);
         tween.SetTrans(Tween.TransitionType.Quart);
-        tween.TweenProperty(this, "position", finalPos, 2f);
+        tween.TweenProperty(this, "position", finalPosition, 2f);
     }
 }
