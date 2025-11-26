b2917869933dd1d7b34c80474a8b8c37052b46f6
dfcut8 <dfcut8@proton.me>
2025-11-26 17:44:38 -0500

    looks even better now

--- Main.cs
+++ Main.cs
@@ -13,8 +13,25 @@ public partial class Main : Node2D
     private void GetTileMapChildren()
     {
         var tmlChildren = tileMapLayer.GetChildren();
+
+        // Sort children by their tilemap coordinates
+        var sortedChildren = new System.Collections.Generic.List<Node>(tmlChildren);
+        sortedChildren.Sort(
+            (a, b) =>
+            {
+                var nodeA = a as Node2D;
+                var nodeB = b as Node2D;
+                var coordA = tileMapLayer.LocalToMap(nodeA.Position);
+                var coordB = tileMapLayer.LocalToMap(nodeB.Position);
+
+                // Sort by Y first, then X (reversed)
+                int yCompare = coordB.Y.CompareTo(coordA.Y);
+                return yCompare != 0 ? yCompare : coordB.X.CompareTo(coordA.X);
+            }
+        );
+
         var delay = 0.0f;
-        foreach (var child in tmlChildren)
+        foreach (var child in sortedChildren)
         {
             if (child is Tile tile)
             {
